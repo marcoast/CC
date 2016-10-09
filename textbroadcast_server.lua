6ae098214b3b89f594ab6c47ebe03a96f38748c1
marcoast <mrcs.sndrs@gmail.com>
2016-10-09 11:21:40 -0500

    Began creating pseudocode for Text Wireless Broadcasting System (Server)  idea

---
+++ textbroadcast_server.lua
@@ -0,0 +1,7 @@
+-- ** Text Wireless Broadcasting System (Server) ** --
+
+-- User inputs text and the computer stores the text in memory.
+-- User inputs text color and text size.
+-- Computer looks for wireless modems on the computer and wraps them.
+-- Computer opens modem to send channel, with a different reply channel for feedback from client computers.
+-- Computer transmits user-inputted text to all modems within that send channel. Client computer should take these variables and "wrap" them within the monitor output code.
