a577f3259b72b44733f23943549707c8e8bc62bd
marcoast <mrcs.sndrs@gmail.com>
2016-10-12 05:40:22 -0500

    Changed to shell.run
    Trying to load colors API by using shell.run to run as a "program"

--- msg-client.lua
+++ msg-client.lua
@@ -23,7 +23,7 @@ end
 function screen() -- Prints the received message onto the monitor
   local size = data[2]
   local color = data[3]
-  os.loadAPI("/rom/apis/colors") -- Loads the color API
+  shell.run("/rom/apis/colors") -- Loads the color API
   mon.setTextScale(size)
   mon.setTextColor(color)
   local mon1 = term.redirect(mon)
