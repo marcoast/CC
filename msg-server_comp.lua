613e0d94ad8134862aecdfa5c7ed39270fb2f115
marcoast <mrcs.sndrs@gmail.com>
2016-10-12 07:18:05 -0500

    Ported current code for computer block from portable version

--- msg-server_comp.lua
+++ msg-server_comp.lua
@@ -6,6 +6,38 @@ modem.open(281,713,832)
 
 function sending()
 while true do
+write("Enter message: ")
+msg = read()
+write("Enter text size, min. 0.5, incr. 0.5: ")
+size = tonumber(read())
+write("Enter text color: ")
+col = read()
+  if col == "" then
+    col = "white"
+  else
+    col = col
+  end
+modem.transmit(281,713,{msg,size,"colors."..col})
+print("Sent '"..msg.."'")
+sleep(2)
+end
+end
+
+function feedback()
+while true do
+local event,modemSide,senderChannel,replyChannel,message,senderDistance = os.pullEvent("modem_message")
+print("Message received on channel #"..senderChannel)
+end 
+end
+
+parallel.waitForAll(sending,feedback)term.clear()
+term.setCursorPos(1,1)
+
+modem = peripheral.wrap("top")
+modem.open(281,713,832)
+
+function sending()
+while true do
   write("Enter message: ")
   msg = read()
   write("Enter text size, min. 0.5, incr. 0.5: ")
