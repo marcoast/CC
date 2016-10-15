d1e2692acdb1e5d7ab1355aac34918f7dc5d6128
MarCoast <marcoast@users.noreply.github.com>
2016-10-14 21:01:27 -0500

    Added "
    Forgot to add a quotation mark

--- Misc/findfiles-upload.lua
+++ Misc/findfiles-upload.lua
@@ -1,6 +1,6 @@
 local docs = fs.open("/saved/helpdocs","w")
 local find = "data = " .. textutils.serialize( fs.find("*/help/*") )
-local pastes = fs.open(/saved/pastes","w")
+local pastes = fs.open("/saved/pastes","w")
 
 
 docs.writeLine(find)
