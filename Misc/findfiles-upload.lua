151357f60b763005d1debeb33032d9e22aa68e63
marcoast <mrcs.sndrs@gmail.com>
2016-10-15 09:30:41 -0500

    Added unserialize for "data" table reading. Working on reading "data" table from the saved results file.

--- Misc/findfiles-upload.lua
+++ Misc/findfiles-upload.lua
@@ -1,12 +1,23 @@
-local docs = fs.open("/saved/helpdocs","w")
+local docw = fs.open("/saved/helpdocs","w")
 local docr = fs.open("/saved/helpdocs","r")
 local finder = "data = " .. textutils.serialize( fs.find("*/help/*") )
+local  
 local pastes = fs.open("/saved/pastes","w")
 
-docs.writeLine( textutils.serialize(finder) )
-docs.close()
-for i=1,10 do
-	print("Result #" .. i)
-	shell.run("pastebin","put",data[i])
-	os.sleep(20)
+function findSave()
+	fs.makeDir("/saved")
+	docw.WriteLine( finder )
+	docw.close
 end
+
+function pasting()
+	for i=1,10 do
+		print("Result #" .. i)
+		shell.run("pastebin","put", textutils.unserialize( data[i]) )
+		os.sleep(20)
+	end
+end
+
+term.clear
+fs.delete("/saved")
+findSave()
