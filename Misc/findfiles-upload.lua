9c68dba068dd56e99cb54e62cc1a980ad72d2131
marcoast <mrcs.sndrs@gmail.com>
2016-10-14 20:25:43 -0500

    Working towards saving pastebin codes

--- Misc/findfiles-upload.lua
+++ Misc/findfiles-upload.lua
@@ -1,5 +1,13 @@
 local docs = fs.open("/saved/helpdocs","w")
 local find = "data = " .. textutils.serialize( fs.find("*/help/*") )
+local pastes = fs.open(/saved/pastes","w")
+
 
 docs.writeLine(find)
 docs.close()
+for i=1,10 do
+	print("Result #" .. i)
+	shell.run("pastebin","put",data[1])
+
+	os.sleep(20)
+end
