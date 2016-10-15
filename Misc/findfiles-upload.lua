ac06fa04a2d87ffd8bd6540a97ba334b128502f3
MarCoast <marcoast@users.noreply.github.com>
2016-10-14 21:43:56 -0500

    Readded serialize and fixed code
    Named the results as "find" directly instead of saving the variable within the file

--- Misc/findfiles-upload.lua
+++ Misc/findfiles-upload.lua
@@ -1,14 +1,13 @@
 local docs = fs.open("/saved/helpdocs","w")
 local docr = fs.open("/saved/helpdocs","r")
-local find = "data = " .. fs.find("*/help/*")
+local find = textutils.serialize( fs.find("*/help/*") )
 local pastes = fs.open("/saved/pastes","w")
 
 docs.writeLine(find)
 docs.close()
-docr.readAll()
 for i=1,10 do
 	print("Result #" .. i)
-	shell.run("pastebin","put",data[i])
+	shell.run("pastebin","put",find[i])
 	local code = ( ... )
 	print("Debug 'code': " .. code)
 	os.sleep(20)
