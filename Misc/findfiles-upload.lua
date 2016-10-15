7d9ba87b2fe1970e761045acab50b631a649a7d7
marcoast <mrcs.sndrs@gmail.com>
2016-10-14 21:26:09 -0500

    Added file read Allows for "data" table to be read for the pastebin program

--- Misc/findfiles-upload.lua
+++ Misc/findfiles-upload.lua
@@ -1,10 +1,11 @@
 local docs = fs.open("/saved/helpdocs","w")
+local docr = fs.open("/saved/helpdocs","r")
 local find = "data = " .. textutils.serialize( fs.find("*/help/*") )
 local pastes = fs.open("/saved/pastes","w")
 
-
 docs.writeLine(find)
 docs.close()
+docr.readAll()
 for i=1,10 do
 	print("Result #" .. i)
 	shell.run("pastebin","put",data[i])
