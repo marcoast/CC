52d93c27d163fd56745cca056fc716442fea898d
MarCoast <marcoast@users.noreply.github.com>
2016-10-14 22:14:26 -0500

    Fixing table code for file write/read
    Getting nil value error

--- Misc/findfiles-upload.lua
+++ Misc/findfiles-upload.lua
@@ -1,13 +1,12 @@
 local docs = fs.open("/saved/helpdocs","w")
 local docr = fs.open("/saved/helpdocs","r")
-local finder = fs.find("*/help/*")
+local finder = "data = " .. textutils.serialize( fs.find("*/help/*") )
 local pastes = fs.open("/saved/pastes","w")
 
 docs.writeLine( textutils.serialize(finder) )
 docs.close()
-local data = docr
 for i=1,10 do
 	print("Result #" .. i)
-	shell.run("pastebin","put",finder[i])
+	shell.run("pastebin","put",data[i])
 	os.sleep(20)
 end
