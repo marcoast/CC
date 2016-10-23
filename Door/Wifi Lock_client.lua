dcc25042162fd2c98fc15f299cb2099050832980
marcoast <mrcs.sndrs@gmail.com>
2016-10-23 06:14:31 -0500

    Updated program Should work, untested at the moment. Had to rewrite modem/lock code from scratch

--- Door/Wifi Lock_client.lua
+++ Door/Wifi Lock_client.lua
@@ -1,2 +1,18 @@
 local wifi = peripheral.wrap("back")
 local side = "bottom"
+local far = 8
+--------
+local recv = 100
+
+while true do 
+	wifi.open(100)
+	data = { os.pullEvent("modem_message") }
+	msg = data[5]
+	dist = tonumber(data[6])
+	if ( dist < far  or msg == "open" ) then
+		rs.setOutput(side,true)
+		sleep(2)
+	elseif dist > far
+		rs.setOutput(side,false)
+	end
+end
